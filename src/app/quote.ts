c601783ca3404077088c8ef8504dc0e91c38c1bc
yvonne <yvonnegax98@gmail.com>
2019-04-30 04:00:09 +0300

    imported class quotes from ../quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    constructor(
+        public id : number,
+        public quoted : string,
+        public author : string
+    ) {}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1,"Respect","Job"),
+    new Quote(2,"Respect","Job"),
+    new Quote(3,"Respect","Job"),
+  ]
 
   constructor() { }
 
